5d3f06e052c4a2b19f562ab3a46b9059fcb1fa65
Gray Lloyd <362572141@GoTVDSB.ca>
2025-05-15 14:43:58 -0400

    N

--- Game1.cs
+++ Game1.cs
@@ -9,7 +9,24 @@ namespace Summative_Animation
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
+        Rectangle window;
+
         Texture2D russiaFlag;
+        Texture2D unacIntercept;
+        Rectangle unacIntRect;
+        Texture2D Intercept;
+
+        Texture2D back1;
+        Texture2D back2;
+        Texture2D milIntro;
+
+        enum Screen
+        {
+            Intro,
+            Military
+        }
+        Screen screen;
+        MouseState mouseState;
 
         public Game1()
         {
@@ -20,7 +37,13 @@ namespace Summative_Animation
 
         protected override void Initialize()
         {
+
+            _graphics.PreferredBackBufferWidth = 800;
+            _graphics.PreferredBackBufferHeight = 500;
+            _graphics.ApplyChanges();
+
             // TODO: Add your initialization logic here
+            screen = Screen.Intro;
 
             base.Initialize();
         }
@@ -28,14 +51,24 @@ namespace Summative_Animation
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
+            milIntro = Content.Load<Texture2D>("milIntro");
+            milIntro = Content.Load<Texture2D>("Intercept");
+            milIntro = Content.Load<Texture2D>("unacIntercept");
 
             // TODO: use this.Content to load your game content here
         }
 
         protected override void Update(GameTime gameTime)
         {
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))               
                 Exit();
+
+            mouseState = Mouse.GetState();
+            if (screen == Screen.Intro)
+            {
+                if (mouseState.LeftButton == ButtonState.Pressed)
+                    screen = Screen.Military;
+            }
 
             // TODO: Add your update logic here
 
@@ -45,6 +78,17 @@ namespace Summative_Animation
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.RoyalBlue);
+
+            _spriteBatch.Begin();
+            if (screen == Screen.Intro)
+            {
+                _spriteBatch.Draw(milIntro, new Rectangle(0, 0, 800, 500), Color.White);
+            }
+            else if (screen == Screen.Military)
+            {
+                _spriteBatch.Draw(unacIntercept, unacIntRect, Color.White);
+            }
+            _spriteBatch.End();
 
             // TODO: Add your drawing code here
 
